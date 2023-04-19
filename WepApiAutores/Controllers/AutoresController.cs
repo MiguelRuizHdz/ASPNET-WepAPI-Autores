00e86462bdf65d1a6d75219ae373ae0789cfdb66
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2023-04-19 15:57:47 -0600

    Iniciando el Desarrollo del Web API

---
+++ WepApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WepApiAutores.Entidades;
+
+namespace WepApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet] 
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>() { 
+                new Autor() { Id = 1, Nombre = "Miguel" },
+                new Autor() { Id = 2, Nombre = "Jorge" } 
+            };
+        }
+    }
+}
